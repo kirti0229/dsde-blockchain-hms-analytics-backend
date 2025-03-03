ca1f5df71facf36e6a077ac534d3a9dac67dc0f7
Kirti Das <kirtidas90547@gmail.com>
2025-03-03 12:25:00 +0530

    BEST001:New User Table Creation

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
+
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const connectDB = require('./config/db');
-const authRoutes = require('./routes/authRoutes');
+
 
 const app = express();
 
@@ -12,11 +11,8 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-// Connect to Database
-connectDB();
 
-// Routes
-app.use('/api/auth', authRoutes);
+
 
 // Start Server
 const PORT = process.env.PORT || 5000;
